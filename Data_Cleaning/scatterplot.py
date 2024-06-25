bfaea0325b51714ecbbdf2a7e03a48fe278183f7
ghernandez <ghernandez@cxc.com.mx>
2024-06-24 22:55:08 -0600

    refactor(files)

---
+++ Data_Cleaning/scatterplot.py
@@ -0,0 +1,32 @@
+import gc
+import os
+import sys
+from pathlib import Path
+
+import dotenv
+import pandas as pd
+
+
+class Scatterplot:
+
+    def __init__(self, env_path: Path = Path('../.env')):
+        self.env_path = env_path
+        dotenv.load_dotenv(dotenv_path=self.env_path)
+
+    @staticmethod
+    def load_file_to_dataframe(sep: str = ',', skip: int = 0) -> pd.DataFrame:
+        path_file = os.getenv('TXT')
+        return pd.read_csv(path_file, sep=sep, skiprows=skip)
+
+    def scatterplot(self):
+        data = self.load_file_to_dataframe()
+        data.plot(kind="scatter", x="Day Mins", y="Day Charge")
+
+
+if __name__ == "__main__":
+    try:
+        reader = Scatterplot()
+        reader.scatterplot()
+        gc.enable()
+    except KeyboardInterrupt:
+        sys.exit()
